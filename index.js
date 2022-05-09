ddf661144720ac00accea29fb0e28e734fa4d1b1
Angelina <angelinarwhite@gmail.com>
2022-05-09 16:58:02 -0400

    added index.js and added fetch to api

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => 
+{
+    fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json")
+    .then((response) => response.json())
+    .then((element) => console.log(element.eur))
+})
