51b4fc69b0540c16ca0b40efb38faa0cc67f1f47
Angelina <angelinarwhite@gmail.com>
2022-05-10 10:10:09 -0400

    add displayCountryRate() and calculateRate()

--- index.js
+++ index.js
@@ -2,5 +2,37 @@ document.addEventListener('DOMContentLoaded', (event) =>
 {
     fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json")
     .then((response) => response.json())
-    .then((element) => console.log(element.eur))
+    .then((countryExchange) => 
+    {
+        displayCountryRate(countryExchange)
+        calculateRate(countryExchange)
+    })
+    
 })
+
+
+
+
+
+
+
+
+
+
+function displayCountryRate(country)
+{
+    const amount = document.getElementById("amount")
+    console.log(amount)
+    console.log(country)
+}
+
+function calculateRate(country)
+{
+    console.log(country)
+    const form = document.getElementById("calculator-form")
+    form.addEventListener("submit", (event) =>
+    {
+        event.preventDefault()
+        console.log("clicky")
+    })
+}
